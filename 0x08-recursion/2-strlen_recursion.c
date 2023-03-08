11b6e604d711419f1c3579bff526e458499a0465
Margaretndungu <mnjoki.ndungu05@gmail.com>
2023-03-08 13:45:24 -0800

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen_recursion - function to return length of a string
+ * @s:string
+ * Return: Always 0 (success)
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}

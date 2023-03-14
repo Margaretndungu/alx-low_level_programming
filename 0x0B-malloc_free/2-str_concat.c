cf6746c5a785f28e64c0b47edb8270e21798b6ce
Margaretndungu <mnjoki.ndungu05@gmail.com>
2023-03-14 05:21:50 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,9 +11,9 @@ char *str_concat(char *s1, char *s2)
 	char *k;
 	int i, j, r, l;
 
-	if (s1 == 0)
+	if (s1 == NULL)
 		s1 = " ";
-	if (s2 == 0)
+	if (s2 == NULL)
 		s2 = " ";
 	for (i = 0; s1[i] != '\0'; i++)
 		;

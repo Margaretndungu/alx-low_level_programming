8ff4f086a3805a957f9c3b4ca40de64e229658cb
Margaretndungu <mnjoki.ndungu05@gmail.com>
2023-03-21 02:55:18 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (EXIT_SUCCESS);
+}

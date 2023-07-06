b970c3a350ca876073bd3c179c5ad145f34e9b7c
Margaretndungu <mnjoki.ndungu05@gmail.com>
2023-07-06 07:28:01 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,6 +9,12 @@ void print_binary(unsigned long int n)
 	unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
 	int leadingzeros = 1;
 
+	if (n == 0)
+	{
+		putchar('0');
+		return;
+	}
+
 	while (mask > 0)
 	{
 		if (n & mask)

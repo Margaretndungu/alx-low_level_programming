61503e310d328aa96ca8254c2344c12e222bdb18
Margaretndungu <mnjoki.ndungu05@gmail.com>
2023-07-06 06:40:33 -0700

    binary_to_uint

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * binary_to_unit - function that converts a binary number to an unsigned int
+ * @b:pointer to the string
+ * Return:0 or 1
+ */
+unsigned int binary_to_unit(const char *b)
+{
+	unsigned int result = 0;
+	int  i;
+
+	if (b == NULL)
+	{
+		return 0;
+	}
+
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] == '0')
+		{
+			result = result * 2;
+		}
+		else if (b[i] == '1')
+		{
+			result = result *2 + 1;
+		}
+		else
+		{
+			return 0;
+		}
+	}
+	return result;
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,9 @@
+0x14. C - Bit manipulation
+
+bit manipulation involves manipulation of individual bits within binary representation of data. it allows efficient handling of binary data, bitwise operations and compact storage of flag s or boolean values
+
+data is typically stored in binary format, with each bit representing a binary digit(0 or 1).
+
+the most commonly used bitwise operations are:
+AND(&),OR(|), XOR(^) and NOT(~)
+
